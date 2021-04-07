3d6d9d20a487a74079599d45aebe24c8b97bf562
Your God <ass@hole>
2021-04-07 16:38:22 +0500

    sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import modals from "./modules/modals";
+import sliders from "./modules/sliders";
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -6,4 +7,6 @@ window.addEventListener('DOMContentLoaded', () => {
     modals('.button-design', '.popup-design', '.popup-close');
     modals('.button-consultation', '.popup-consultation', '.popup-close');
     modals('.fixed-gift', '.popup-gift', '.popup-gift .popup-close', true);
+    sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
+    sliders('.main-slider-item', 'vertical');
 });
